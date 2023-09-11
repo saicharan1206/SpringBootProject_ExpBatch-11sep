5f0c80979c358e2f296f3ec2047248a0c23511c8
mishrasai1206 <mishrasai1206@gmail.com>
2023-09-11 12:09:37 +0530

    Project Initiated

---
+++ src/main/java/com/jspiders/project/SpringBootProjectExpBatch11SeptApplication.java
@@ -0,0 +1,13 @@
+package com.jspiders.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectExpBatch11SeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectExpBatch11SeptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jspiders/project/SpringBootProjectExpBatch11SeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspiders.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectExpBatch11SeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

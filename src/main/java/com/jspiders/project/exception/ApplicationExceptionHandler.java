6778fff03a777b06e68d05bfb9190a690b04e6e7
VIJAYKUMAR-N-I <16vjvijay8055@gmail.com>
2023-09-11 23:36:56 +0530

    first project

--- src/main/java/com/jspiders/project/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/jspiders/project/exception/ApplicationExceptionHandler.java
@@ -1,5 +1,45 @@
 package com.jspiders.project.exception;
 
-public class ApplicationExceptionHandler {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.jspiders.project.utility.ErrorStructure;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		List<ObjectError> allErrors = ex.getAllErrors();
+		Map<String, String> errors = new HashMap<>();
+
+		for(ObjectError error : allErrors) {
+			FieldError fieldError = (FieldError)error;
+			String field = fieldError.getField();
+			String message = fieldError.getDefaultMessage();
+			errors.put(field, message);
+		}
+		return new ResponseEntity<Object>(errors,HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler
+	public ResponseEntity<ErrorStructure> userAlreadyRegistered(UserExistsException exception){
+		ErrorStructure es = new ErrorStructure();
+		es.setStatusCode(HttpStatus.NOT_FOUND.value());
+		es.setMessage(exception.getMessage());
+		es.setRootCause("User Already Registered!!!");
+		return new ResponseEntity<ErrorStructure>(es, HttpStatus.NOT_FOUND);
+	}
 }
--- src/main/java/com/jspiders/project/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/jspiders/project/serviceimpl/UserServiceImpl.java
@@ -1,7 +1,49 @@
 package com.jspiders.project.serviceimpl;
 
-import com.jspiders.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import com.jspiders.project.dto.UserRequestDTO;
+import com.jspiders.project.dto.UserResponseDTO;
+import com.jspiders.project.entities.User;
+import com.jspiders.project.exception.UserExistsException;
+import com.jspiders.project.repo.UserRepo;
+import com.jspiders.project.service.UserService;
+import com.jspiders.project.utility.ResponseStructure;
+@Service
 public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepo userRepo;
+	@Override
+	public ResponseEntity<ResponseStructure<UserResponseDTO>> saveUser(UserRequestDTO userRequestDTO) {
+		User save;
+		User user=new User();
+		if(userRepo.findByUserEmail(userRequestDTO.getUserEmail())==null){
+			user.setUserEmail(userRequestDTO.getUserEmail().toLowerCase());
+			user.setUserPassword(userRequestDTO.getUserPassword());
+			save = userRepo.save(user);
+			System.out.println(user.toString());
+		}
+		else {
+			throw new UserExistsException("User already registered");
+		}
+		
+		UserResponseDTO responseDTO = new UserResponseDTO();
+		responseDTO.setUserId(save.getUserId());
+		responseDTO.setUserEmail(save.getUserEmail());
+		responseDTO.setCreatedBy(save.getCreatedBy());
+		responseDTO.setCreatedDate(save.getCreatedDate());
+		responseDTO.setUpdatedBy(save.getUpdatedBy());
+		responseDTO.setUpdatedDate(save.getUpdatedDate());
+		
+		ResponseStructure<UserResponseDTO> responseStructure = new ResponseStructure<>();
+		responseStructure.setData(responseDTO);
+		responseStructure.setMessage("User registered successfully");
+		responseStructure.setStatusCode(HttpStatus.CREATED.value());
+		
+		return new ResponseEntity<ResponseStructure<UserResponseDTO>>(responseStructure, HttpStatus.CREATED);
+	}
 	
 }

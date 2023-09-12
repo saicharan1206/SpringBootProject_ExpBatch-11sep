3885840d692c780e5b7145cb81d5a7eafa0ae100
Mohammed143-coder <junaithdedhai2001@gmail.com>
2023-09-12 12:40:39 +0530

    new cmt

--- src/main/java/com/jspiders/project/utility/AuditConfig.java
+++ src/main/java/com/jspiders/project/utility/AuditConfig.java
@@ -1,15 +1,18 @@
 package com.jspiders.project.utility;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "aware")
 public class AuditConfig {
+	
 	@Bean
-	public AuditorAware<String> auditorAware(){
-		return new AuditorAwareImpl();
+	public AuditorAware<String>aware(){
+		return ()->Optional.of("junaith");
 	}
 }
